276c6766899ed0a83f8d4430853f306c38cfd9ed
Daniel Smit <daniel.smit.nl@gmail.com>
2024-01-08 09:21:00 +0100

    Functional design app 1

---
+++ assessment.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import regex as re
+import matplotlib.pyplot as plt
+
+class BioAPP:
+    def __init__(self, master):
+
+
+    def load_input_file(file_path):
+
+
+    def find_consensus_sequences(self, sequences):
+        # Zoeken naar consensus sequenties met regex
+
+
+    def analyze_data(self, option):
+        # Implementeer de gewenste analyse-opties (a, b, c)
+
+
+    def plot_data(self, option):
+        # Implementeer de gewenste plot-opties (a, b)
+
+
+
+
+# Initialisatie
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BioAPP(root)
+    root.mainloop()
